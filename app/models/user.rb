e9b1063b1d74248e68cfe7e13fbde6b1a6a58608
Addis Belete <addis.bel@gmail.com>
2021-05-12 22:59:27 +0300

    Add assocation between vote user and vehicle model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :vehicles
+  has_many :votes
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,7 @@
 class Vehicle < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  has_many :votes
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
