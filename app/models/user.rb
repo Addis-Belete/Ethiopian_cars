974d00d2f507ac207123e7b0c448e66e93a55129
Addis Belete <addis.bel@gmail.com>
2021-05-14 09:17:37 +0300

    Add more validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :vehicles, dependent: :destroy
   has_many :votes, dependent: :destroy
+  validates_associated :vehicles
+  validates_associated :votes
+  validates :email, uniqueness: true
 
   def votes?(vehicle)
     vehicle.votes.where(user_id: id).any?
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -5,6 +5,9 @@ class Vehicle < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates :make, :model, :fuel_type, :name, :vehicle_conditions, presence: true
+  validates_associated :votes
+  validates :vehicle_conditions, length: { minimum: 10 }
+  validates :user, :category, presence: true
 
   def voted?(user)
     !!self.votes.find { |like| like.user_id == user.id }
