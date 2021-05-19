88067798819733c446171c7755fa25b3b56b0a08
Addis Belete <addis.bel@gmail.com>
2021-05-19 12:10:43 +0300

    remove logic from view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def like(vehicle)
+    user_signed_in?
+    if vehicle.voted?(current_user)
+      button_to "Unlike", unvote_path(@vehicle), method: "put"
+    else
+      button_to "Like", vote_path(@vehicle), method: "put"
+    end
+  end
 end
