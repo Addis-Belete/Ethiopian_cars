1b8ba0d8624539bcb257309a97e60dd8e941bc4e
Addis Belete <addis.bel@gmail.com>
2021-06-15 21:30:15 +0300

    Add link preview

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,29 @@ module ApplicationHelper
       link_to 'Back', vehicles_path
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'MyMovies.dev',
+      title: 'MyMovies',
+      reverse: true,
+      separator: '|',
+      description: 'The most popular movie website in the world',
+      keywords: 'action, horror, drama',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
+      ],
+      og: {
+        site_name: 'MyMovies.dev',
+        title: 'MyMovies',
+        description: 'The most popular movie website in the world',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ethio_cars3.png')
+      }
+    }
+  end
 end
